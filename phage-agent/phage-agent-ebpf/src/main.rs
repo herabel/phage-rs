4d0819cf1a14460f71e9b5c79f888eabe59e5034
herabel <3423.0a@mail.ru>
2026-07-23 11:55:14 +0300

    feat(ebpf): add new tracepoint targets (unlinkat, connect, bind, accept)

--- phage-agent/phage-agent-ebpf/src/main.rs
+++ phage-agent/phage-agent-ebpf/src/main.rs
@@ -8,6 +8,8 @@ use aya_ebpf::helpers::bpf_probe_read_user_str_bytes;
 use aya_log_ebpf::info;
 mod data_helpers;
 
+// Files
+//////////////////////////////////////////////////////////////////////////
 
 #[tracepoint(category = "syscalls", name = "sys_enter_execve")]
 pub fn sys_enter_execve(ctx: TracePointContext) -> i32 {
@@ -81,6 +83,102 @@ fn try_sys_enter_openat(ctx: TracePointContext) -> Result<i32, i32> {
     }
     Ok(0)
 }
+
+#[tracepoint(category = "syscalls", name = "sys_enter_unlinkat")]
+pub fn sys_enter_unlinkat(ctx: TracePointContext) -> i32 {
+    try_sys_enter_unlinkat(ctx).unwrap_or_else(|ret| ret)
+}
+
+fn try_sys_enter_unlinkat(ctx: TracePointContext) -> Result<i32, i32> {
+    let (pid, comm) = data_helpers::get_process_info().unwrap_or((0, [0u8; 16]));
+
+    let mut len = 0;
+    while len < comm.len() && comm[len] != 0 {
+        len += 1;
+    }
+    let comm_str = unsafe { core::str::from_utf8_unchecked(&comm[..len]) };
+
+    if let Ok(filename_ptr) = unsafe { ctx.read_at::<*const u8>(24) } {
+        let mut path_buf = [0u8; 128];
+
+        if let Ok(path_bytes) = unsafe { bpf_probe_read_user_str_bytes(filename_ptr, &mut path_buf) } {
+            let path_str = unsafe { core::str::from_utf8_unchecked(path_bytes) };
+
+            if !data_helpers::is_blacklisted(comm_str) {
+                info!(&ctx, "PID: {}, Unlink: {}, Path: {}", pid, comm_str, path_str);
+            }
+        }
+    }
+    Ok(0)
+}
+
+// Network
+//////////////////////////////////////////////////////////////////////////
+#[tracepoint(category = "syscalls", name = "sys_enter_connect")]
+pub fn sys_enter_connect(ctx: TracePointContext) -> i32 {
+    try_sys_enter_connect(ctx).unwrap_or_else(|ret| ret)
+}
+
+fn try_sys_enter_connect(ctx: TracePointContext) -> Result<i32, i32> {
+    let (pid, comm) = data_helpers::get_process_info().unwrap_or((0, [0u8; 16]));
+
+    let mut len = 0;
+    while len < comm.len() && comm[len] != 0 {
+        len += 1;
+    }
+    let comm_str = unsafe { core::str::from_utf8_unchecked(&comm[..len]) };
+
+    if !data_helpers::is_blacklisted(comm_str) {
+        info!(&ctx, "PID: {}, Connection event from: {}", pid, comm_str);
+    }
+    Ok(0)
+}
+
+#[tracepoint(category = "syscalls", name = "sys_enter_bind")]
+pub fn sys_enter_bind(ctx: TracePointContext) -> i32 {
+    try_sys_enter_bind(ctx).unwrap_or_else(|ret| ret)
+}
+
+fn try_sys_enter_bind(ctx: TracePointContext) -> Result<i32, i32> {
+    let (pid, comm) = data_helpers::get_process_info().unwrap_or((0, [0u8; 16]));
+
+    let mut len = 0;
+    while len < comm.len() && comm[len] != 0 {
+        len += 1;
+    }
+    let comm_str = unsafe { core::str::from_utf8_unchecked(&comm[..len]) };
+
+    if !data_helpers::is_blacklisted(comm_str) {
+        info!(&ctx, "PID: {}, Bind event from: {}", pid, comm_str);
+    }
+    Ok(0)
+}
+
+#[tracepoint(category = "syscalls", name = "sys_enter_accept")]
+pub fn sys_enter_accept(ctx: TracePointContext) -> i32 {
+    try_sys_enter_accept(ctx).unwrap_or_else(|ret| ret)
+}
+
+fn try_sys_enter_accept(ctx: TracePointContext) -> Result<i32, i32> {
+    let (pid, comm) = data_helpers::get_process_info().unwrap_or((0, [0u8; 16]));
+
+    let mut len = 0;
+    while len < comm.len() && comm[len] != 0 {
+        len += 1;
+    }
+    let comm_str = unsafe { core::str::from_utf8_unchecked(&comm[..len]) };
+
+    if !data_helpers::is_blacklisted(comm_str) {
+        info!(&ctx, "PID: {}, Accept connection event from: {}", pid, comm_str);
+    }
+
+    Ok(0)
+}
+
+// Dangerous
+//////////////////////////////////////////////////////////////////////////
+
+
 
 #[cfg(not(test))]
 #[panic_handler]
