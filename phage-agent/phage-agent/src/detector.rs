16cc69a9fa46d57dad4573dfd9fb86f1edf91592
herabel <3423.0a@mail.ru>
2026-08-24 21:29:19 +0300

    feat(detector): implement real-time threats detector
     - Add WebShell detection for web servers (MITRE ATT&CK T1505.003 / T1190)
     - Prevent execution from /dev/shm/, /tmp/ and memfd (MITRE ATT&CK T1027.004 / T1564.001)

---
+++ phage-agent/phage-agent/src/detector.rs
@@ -0,0 +1,34 @@
+use phage_agent_common::SyscallEvent;
+
+pub enum Decision {
+    Allow,
+    Deny { reason: &'static str },
+}
+
+pub fn evaluate(event: &SyscallEvent) -> Decision {
+    let path  = event.filename_str();
+    let _args = event.args_bytes();
+
+    let comm_end = event.comm.iter().position(|&c| c == 0).unwrap_or(event.comm.len());
+    let comm = std::str::from_utf8(&event.comm[..comm_end]).unwrap_or("<invalid utf8>");
+
+    let let_is_web_server = comm.starts_with("nginx") || comm.starts_with("apache") || comm.starts_with("caddy");
+    let is_shell = path.ends_with("/sh") || path.ends_with("/bash") || path.ends_with("/dash");
+    if let_is_web_server && is_shell {
+        return Decision::Deny{
+            reason: "Web Server spawned shell (Webshell attack | MITRE ATT&CK: T1505.003 / T1190)",
+        };
+    }
+
+    if path.starts_with("/dev/shm/")
+        || path.starts_with("/tmp/")
+        || path.starts_with("/var/tmp/")
+        || path.starts_with("memfd:")
+        || path.starts_with("/proc/self/fd/"){
+        return Decision::Deny{
+            reason: "Executable launched from shared memory (In-memory payload | MITRE ATT&CK: T1027.004 / T1564.001)"
+        }
+    }
+
+    Decision::Allow
+}
