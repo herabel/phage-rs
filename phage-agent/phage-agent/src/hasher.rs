8c46526cfe5ab047ce0340756933f1955cb3b28a
herabel <3423.0a@mail.ru>
2026-08-24 16:22:27 +0300

    feat(hasher): add blake3 hasher

--- phage-agent/phage-agent/src/hasher.rs
+++ phage-agent/phage-agent/src/hasher.rs
@@ -1,1 +1,11 @@
-use sha2;
+use blake3;
+use std::fs::File;
+use std::path::{Path};
+
+pub fn hash(path: &Path) -> Result<[u8;32], std::io::Error> {
+    let file = File::open(path)?;
+    let mut hasher = blake3::Hasher::new();
+    hasher.update_reader(&file)?;
+
+    Ok(*hasher.finalize().as_bytes())
+}
